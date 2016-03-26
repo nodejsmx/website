3933e0d764a4d06e51e770624f5a9187874ebe93
Luis <luis@x8bit.com>
2016-03-26 14:21:50 -0600

    added meetups

---
+++ api/controllers/MeetupsController.js
@@ -0,0 +1,11 @@
+/**
+ * MeetupsController
+ *
+ * @description :: Server-side logic for managing meetups
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
