8833268007c251011f5dbc7ef12e8e432bd4f0e6
colin <acolin@incaztech.com>
2016-04-06 01:47:19 -0500

    components and map utils to load several elements into a map

---
+++ assets/js/components/tweets.js
@@ -0,0 +1,40 @@
+var addMarkers = require('../map_utils').addMarkers;
+
+module.exports = function(params) {
+  if (typeof params.map !== 'object') {
+    throw new TypeError('params.map must be a map object');
+  }
+
+  _loadTweets(function (tweets) {
+    addMarkers(params.map, tweets);
+  });
+};
+
+function _loadTweets(callback) {
+  $.get('/tweets/read', function(data, status){
+    var tweets = _mapTweets(data.statuses);
+    callback(tweets);
+  });
+}
+
+function _mapTweets(data) {
+  var mapped = [];
+
+  data.forEach(function (tweet) {
+    var place = tweet.place;
+
+    if (place) {
+      var latlng = [
+        place.bounding_box.coordinates[0][0][1],
+        place.bounding_box.coordinates[0][0][0],
+      ];
+
+      mapped.push({
+        image: tweet.user.profile_image_url,
+        point: latlng
+      });
+    }
+  });
+
+  return mapped;
+}
